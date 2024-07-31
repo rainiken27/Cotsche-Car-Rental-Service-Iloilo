c21f7901bd2454184671ab922b4bec6567a6673e
rainiken27 <motrifalgar@up.edu.ph>
2024-07-31 10:04:28 +0800

    ads

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,12 @@
-import { Route, Routes } from "react-router-dom"
-import Home  from "./components/Home.jsx"
-import VehicleList from "./components/vehicle/VehicleList.jsx"
-
+import Header from "./components/Header";
 
 const App = () => {
   return (
-    <Routes>
-      <Route path="/" element={<Home />} />
-      <Route path="/vehicle-list" element={<VehicleList />} />
-    </Routes>
+    <>
+      <div className="pt-[4.75rem] lg:pt-[5.25rem] overflow-hidden">
+        <Header />
+      </div>
+    </>
   );
 };
 
--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,103 +1,24 @@
-import { useLocation } from "react-router-dom";
-import Button from "./Button";
 import logo from "../assets/cotsche_vector.svg";
 import { navigation } from "../constants";
-import MenuSvg from "../assets/svg/MenuSvg";
-import { HamburgerMenu } from "./design/Header";
-import { useState } from "react";
-import { disablePageScroll, enablePageScroll } from "scroll-lock";
-import Overlay from "./Overlay";
-import { HashLink as Link } from "react-router-hash-link";
 
 const Header = () => {
-  const pathname = useLocation();
-  const [openNavigation, setOpenNavigation] = useState(false);
-  const [overlayVisibility, setOverlayVisibility] = useState(false);
-
-
-  const toggleNavigation = () => {
-    if (openNavigation) {
-      setOpenNavigation(false);
-      enablePageScroll();
-    } else {
-      setOpenNavigation(true);
-      disablePageScroll();
-    }
-  };
-
-  const handleClick = () => {
-    if (!openNavigation) return;
-
-    enablePageScroll();
-    setOpenNavigation(false);
-  };
-
-  const openOverlay = () => {
-    setOverlayVisibility(true);
-    disablePageScroll();
-  }
-
-  const closeOverlay = () => {
-    setOverlayVisibility(false);
-    enablePageScroll();
-  }
-
   return (
-    <div
-      className={`${
-        openNavigation ? "bg-n-8" : "bg-n-8/90 backdrop-blur-sm"
-      } fixed top-0 left-0 w-full z-50 border-b border-n-6`}
-    >
+    <div className="fixed top-0 z-50 backgrop-blur-sm border-b border-n-6 lg: bg-n-8/90 lg:backgrop-blur-sm">
       <div className="flex items-center px-1 lg:px-1.5 xl:px-2 max-lg:py-0.8">
-        <Link className="block w-[12.5rem]" to="/#hero">
+        <a className="block w-[20rem] xl:mr-8" href="#hero">
           <img src={logo} width={200} />
-        </Link>
+        </a>
 
-        <nav
-          className={`${
-            openNavigation ? "flex" : "hidden"
-          } fixed top-[3.8rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg:bg-transparent`}
-        >
+        <nav className="hidden fixed top-5[rem] left-0 right-0 bottom-0 bg-n-8 lg:static lg:flex lg:mx-auto lg: bg-transparent">
           <div className="relative z-2 flex flex-col items-center justify-center m-auto lg:flex-row">
-            <div className="absolute -top-32 text-white text-2xl hover:text-color-1 transition-colors duration-200">
-              <a href="https://www.facebook.com/bestcarrentaliloilo/"
-              target="_blank">
-                Contact Us
-              </a>
-            </div>
             {navigation.map((item) => (
-              <Link
-                key={item.id}
-                to={item.url}
-                onClick={handleClick}
-                className={`block relative font-code text-2xl text-n-1 transition-colors hover:text-color-3 ${
-                  item.onlyMobile ? "lg:hidden" : ""
-                } px-5 py-5 lg:-mr-0.25 lg:text-base lg:font-semibold ${
-                  item.url === pathname.hash
-                    ? "z-2 lg:text-n-1"
-                    : "lg:text-n-1/50"
-                } lg:hover:text-n-1`}
-              >
+              <a key={item.id} href={item.url}>
                 {item.title}
-              </Link>
+              </a>
             ))}
           </div>
-          <HamburgerMenu />
         </nav>
-
-        <Button
-
-          isOverlayButton={true}
-          onClick={openOverlay}
-        >
-          Contact Us
-        </Button>
-
-        <Button  onClick={toggleNavigation}>
-          <MenuSvg openNavigation={openNavigation} />
-        </Button>
       </div>
-      {overlayVisibility && <Overlay onClose={closeOverlay} />}
     </div>
   );
 };
